66fd4766bcbbd3a86fd74361e928775c0d4af9f8
Amir <amirhkarimi888@gmail.com>
2023-09-06 11:41:37 +0330

    All operations were defined

--- main.js
+++ main.js
@@ -66,37 +66,101 @@ const nine = () => {
   inputValue = inputValue + "9";
   mainInput.value = inputValue;
 }
+const point = () => {
+  if(!inputValue.includes(".")) {
+    if(inputValue != "") {
+      inputValue = inputValue + ".";
+      mainInput.value = inputValue;
+    } else {
+      inputValue = inputValue + "0.";
+      mainInput.value = inputValue;
+    }
+  }
+}
 
 
 let initial = 0;
 let operation = "";
 
-const plus = () => {
+const sumation = () => {
   initial = parseFloat(inputValue);
 
   inputValue = "";
   operation = "+";
 }
 
-const minus = () => {
+const subtraction = () => {
   initial = parseFloat(inputValue);
 
   inputValue = "";
   operation = "-";
 }
 
-const product = () => {
+const multiplication = () => {
   initial = parseFloat(inputValue);
 
   inputValue = "";
-  operation = "*";
+  operation = "×";
 }
 
-const divide = () => {
+const division = () => {
   initial = parseFloat(inputValue);
 
   inputValue = "";
-  operation = "/";
+  operation = "÷";
+}
+
+const getRoot = () => {
+  initial = parseFloat(inputValue);
+
+  if(initial >= 0) {
+    inputValue = Math.sqrt(initial);
+    mainInput.value = inputValue;
+  }
+}
+
+const power = () => {
+  initial = parseFloat(inputValue);
+
+  inputValue = "";
+  operation = "^";
+}
+
+const remaining = () => {
+  initial = parseFloat(inputValue);
+
+  inputValue = "";
+  operation = "%";
+}
+
+const reverse = () => {
+  initial = parseFloat(inputValue);
+
+  inputValue = 1/initial;
+  mainInput.value = inputValue;
+}
+
+const changeSign = () => {
+  initial = parseFloat(inputValue);
+
+  inputValue = initial * (-1);
+  mainInput.value = inputValue;
+}
+
+const CE = () => {
+  inputValue = "";
+  mainInput.value = "0";
+}
+
+const C = () => {
+  initial = "";
+  inputValue = "";
+  mainInput.value = "0";
+}
+
+const DEL = () => {
+  inputValue = inputValue.slice(0, inputValue.length - 1);
+  mainInput.value = inputValue;
 }
 
 const equal = () => {
@@ -107,11 +171,17 @@ const equal = () => {
     case "-":
       inputValue = initial - parseFloat(inputValue);
       break;
-    case "*":
+    case "×":
       inputValue = initial * parseFloat(inputValue);
       break;
-    case "/":
+    case "÷":
       inputValue = initial / parseFloat(inputValue);
+      break;
+    case "^":
+      inputValue = Math.pow(initial, parseFloat(inputValue));
+      break;
+    case "%":
+      inputValue = initial % parseFloat(inputValue);
       break;
   }
   mainInput.value = inputValue;
@@ -119,30 +189,30 @@ const equal = () => {
 
 
 const buttons = [
-  { sym: "%", func: one },
-  { sym: "rad", func: one },
-  { sym: "^", func: one },
-  { sym: "1/x", func: one },
-  { sym: "CE", func: one },
-  { sym: "C", func: one },
-  { sym: "DEL", func: one },
-  { sym: "/", func: divide },
+  { sym: "%", id: "remaining", func: remaining },
+  { sym: "√", id: "root", func: getRoot },
+  { sym: "^", id: "power", func: power },
+  { sym: "1/x", id: "reverse", func: reverse },
+  { sym: "CE", id: "ce", func: CE },
+  { sym: "C", id: "c", func: C },
+  { sym: "DEL", id: "del", func: DEL },
+  { sym: "÷", id: "division", func: division },
   { sym: "7", id: "btn7", func: seven },
   { sym: "8", id: "btn8", func: eight },
   { sym: "9", id: "btn9", func: nine },
-  { sym: "*", func: product },
+  { sym: "×", id: "multiplication", func: multiplication },
   { sym: "4", id: "btn4", func: four },
   { sym: "5", id: "btn5", func: five },
   { sym: "6", id: "btn6", func: six },
-  { sym: "-", func: minus },
+  { sym: "-", id: "subtraction", func: subtraction },
   { sym: "1", id: "btn1", func: one },
   { sym: "2", id: "btn2", func: two },
   { sym: "3", id: "btn3", func: three },
-  { sym: "+", id: "plus", func: plus },
-  { sym: "+/-", func: one },
-  { sym: "0", func: zero },
-  { sym: ".", func: one },
-  { sym: "=", func: equal },
+  { sym: "+", id: "sumation", func: sumation },
+  { sym: "+/-", id: "changeSign", func: changeSign },
+  { sym: "0", id: "zero", func: zero },
+  { sym: ".", id: "point", func: point },
+  { sym: "=", id: "equal", func: equal },
 ]
 
 buttons.map((button) => {
